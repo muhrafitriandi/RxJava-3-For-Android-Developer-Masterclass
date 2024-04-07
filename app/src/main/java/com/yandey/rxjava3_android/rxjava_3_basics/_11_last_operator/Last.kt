97e069cda46d120f7603b6705b17f17b5ec4ca3e
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-08 05:56:31 +0700

    Last Operator
    - `Observable.last(defaultItem)` is used to retrieve the last item from the Observable and send it to the Observer as a `Single`.
    When we retrieve an item using the `last` operator and it turns out the item is empty, the value in the parameter will become the default item.
    
    - `Observable.lastElement()` is used to retrieve the last item from the Observable and send it to the Observer as `Maybe`.
    If the Observable does not return a value, `Maybe` will be empty.
    The difference with `last()` is that in the `lastElement()` operator we do not need to provide a defaultItem in the parameter.
    
    - `Observable.lastOrError()` is similar to `last()`, but if the Observable does not return a value, it returns `NoSuchElementException`.

---
+++ app/src/main/java/com/yandey/rxjava3_android/rxjava_3_basics/_11_last_operator/Last.kt
@@ -0,0 +1,22 @@
+package com.yandey.rxjava3_android.rxjava_3_basics._11_last_operator
+
+import android.annotation.SuppressLint
+import io.reactivex.rxjava3.core.Observable
+
+@SuppressLint("CheckResult")
+fun main() {
+    val observable1 = Observable.range(1, 10)
+    val observable2 = Observable.fromIterable(emptyList())
+
+    observable1.last(7)
+        .subscribe(
+            { value -> println("Last value: $value") },
+            { error -> println("Error: $error") }
+        )
+
+    observable2.last(7)
+        .subscribe(
+            { value -> println("Last value: $value") },
+            { error -> println("Error: $error") }
+        )
+}
