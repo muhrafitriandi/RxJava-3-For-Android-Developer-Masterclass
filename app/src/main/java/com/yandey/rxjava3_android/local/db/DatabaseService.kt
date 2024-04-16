cdf8c838d48fcb89d4af94a959cb897f44b85b1c
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-16 12:29:44 +0700

    Create Database Instance

--- app/src/main/java/com/yandey/rxjava3_android/local/db/DatabaseService.kt
+++ app/src/main/java/com/yandey/rxjava3_android/local/db/DatabaseService.kt
@@ -1,10 +1,37 @@
 package com.yandey.rxjava3_android.local.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import com.yandey.rxjava3_android.local.dao.StudentDao
 import com.yandey.rxjava3_android.local.entity.StudentEntity
 
 @Database(entities = [StudentEntity::class], version = 1, exportSchema = false)
-abstract class DatabaseService {
+abstract class DatabaseService : RoomDatabase() {
     abstract fun studentDao(): StudentDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DatabaseService? = null
+
+        fun getInstance(context: Context): DatabaseService {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        DatabaseService::class.java,
+                        "student_database"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+
+                    INSTANCE = instance
+                }
+                return instance
+            }
+        }
+    }
 }
