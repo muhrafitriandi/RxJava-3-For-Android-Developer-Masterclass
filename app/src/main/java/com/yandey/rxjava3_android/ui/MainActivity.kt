1d56b01999a2f13e067bb7bc62702fe8208e1e42
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-17 05:00:56 +0700

    Implement ViewModel to Activity

--- app/src/main/java/com/yandey/rxjava3_android/ui/MainActivity.kt
+++ app/src/main/java/com/yandey/rxjava3_android/ui/MainActivity.kt
@@ -2,11 +2,38 @@ package com.yandey.rxjava3_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.yandey.rxjava3_android.R
 
 class MainActivity : AppCompatActivity() {
+    private val mainViewModel: MainViewModel by viewModels {
+        ViewModelFactory.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainViewModel.uiState().observe(this) { uiState ->
+            if (uiState != null) {
+                render(uiState)
+            }
+        }
+    }
+
+    private fun render(uiState: MainUiState) {
+        when (uiState) {
+            is MainUiState.Error -> {
+                // Do Something
+            }
+
+            MainUiState.Loading -> {
+                // Do Something
+            }
+
+            is MainUiState.Success -> {
+                // Do Something
+            }
+        }
     }
 }
