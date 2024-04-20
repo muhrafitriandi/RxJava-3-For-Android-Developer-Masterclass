9c3778719a5c2b8e2ef71b2a4aa28db24a5328e0
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-20 14:26:01 +0700

    Create Project and Dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -45,6 +45,19 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation("io.reactivex.rxjava3:rxjava:3.1.5")
+    // https://github.com/ReactiveX/RxJava
+    implementation("io.reactivex.rxjava3:rxjava:3.1.8")
+
+    // https://github.com/ReactiveX/RxAndroid
     implementation("io.reactivex.rxjava3:rxandroid:3.0.2")
+
+    // https://square.github.io/retrofit/
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    // https://github.com/square/okhttp
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    // https://developer.android.com/jetpack/androidx/releases/activity
+    implementation("androidx.activity:activity-ktx:1.9.0")
 }
