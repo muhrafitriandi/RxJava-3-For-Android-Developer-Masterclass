9126bde85248d50485f65d96a04fdb7d45078d0e
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-21 11:25:18 +0700

    Initialize ViewModel

--- app/src/main/java/com/yandey/rxjava3_android/ui/MainActivity.kt
+++ app/src/main/java/com/yandey/rxjava3_android/ui/MainActivity.kt
@@ -2,11 +2,40 @@ package com.yandey.rxjava3_android.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.yandey.rxjava3_android.R
+import androidx.activity.viewModels
+import com.yandey.rxjava3_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val viewModel: MainViewModel by viewModels {
+        ViewModelFactory.getInstance(this)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        viewModel.uiState().observe(this) { uiState ->
+            if (uiState != null) {
+                render(uiState)
+            }
+        }
+    }
+
+    private fun render(uiState: MainUiState) {
+        when (uiState) {
+            is MainUiState.Error -> {
+                // Do Something
+            }
+            MainUiState.Loading -> {
+                // Do Something
+            }
+            is MainUiState.Success -> {
+                // Do Something
+            }
+        }
     }
 }
