048ff42ad0e95e6adabb069857160e28338ed128
Muhammad Rafi Triandi <61121146+muhrafitriandi@users.noreply.github.com>
2024-04-24 00:48:38 +0700

    ViewModel

--- app/src/main/java/com/yandey/rxjava3_android/ui/MainUiState.kt
+++ app/src/main/java/com/yandey/rxjava3_android/ui/MainUiState.kt
@@ -1,9 +1,10 @@
 package com.yandey.rxjava3_android.ui
 
+import androidx.paging.PagingData
 import com.yandey.rxjava3_android.data.remote.response.QuoteResponseItem
 
 sealed class MainUiState {
     data object Loading : MainUiState()
-    data class Success(val quoteResponse: List<QuoteResponseItem>) : MainUiState()
+    data class Success(val quoteResponse: PagingData<QuoteResponseItem>) : MainUiState()
     data class Error(val message: String) : MainUiState()
 }
--- app/src/main/java/com/yandey/rxjava3_android/ui/MainViewModel.kt
+++ app/src/main/java/com/yandey/rxjava3_android/ui/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.yandey.rxjava3_android.ui
 
+import androidx.lifecycle.viewModelScope
+import androidx.paging.rxjava3.cachedIn
 import com.yandey.rxjava3_android.base.BaseViewModel
 import com.yandey.rxjava3_android.data.repository.QuoteRepository
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class MainViewModel(
     private val quoteRepository: QuoteRepository
@@ -15,10 +18,11 @@ class MainViewModel(
         getQuote()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     private fun getQuote() {
         uiState.value = MainUiState.Loading
 
-        quoteRepository.getQuote()
+        quoteRepository.getQuote().cachedIn(viewModelScope)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
